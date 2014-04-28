b4483536b32ee3f24042e75bde0af0ae968b8bc4
Doug Bell <madcityzen@gmail.com>
2014-04-27 21:46:52 -0500

    fix "Cannot modify private array" in test
    This doesn't appear to be an issue on all versions of Perl

--- t/lib/Greeting.pm
+++ t/lib/Greeting.pm
@@ -3,9 +3,12 @@ use Moo;
 has hello => ( is => 'ro' );
 has who => ( is => 'ro' );
 has default => ( is => 'ro' );
+use diagnostics;
 sub greet {
     my ( $self, @who ) = @_;
-    @who ||= $self->default;
+    if ( !@who ) {
+        @who = ( $self->default );
+    }
     return join ". ", map { sprintf "%s, %s", $self->hello, $_ } @who;
 }
 1;
